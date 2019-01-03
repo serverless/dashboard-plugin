3781314cfe447e2c47c3432e3ee3508bf4f9fedf
Daniel Schep <dschep@gmail.com>
2019-01-03 11:50:38 -0500

    fix lint error with short vars k&v

--- sdk-js/src/lib/transaction.js
+++ sdk-js/src/lib/transaction.js
@@ -185,7 +185,7 @@ class Transaction {
       // Flatten and camelCase schema because EAPM tags are only key/value=string
       // not using lodash's flatten bc its for Arrays and schema is an Object
       let tags = flatten(this.$.schema)
-      tags = _.mapKeys(tags, (v, k) => _.camelCase(k))
+      tags = _.mapKeys(tags, (value, key) => _.camelCase(key))
       tags.traceId = tags.computeCustomAwsRequestId
 
       // if transaction add the request id as the transaction trace
