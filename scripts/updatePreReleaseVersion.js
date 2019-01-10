22282d487abdce874bea3c339580a3112d3c2d7c
Daniel Schep <dschep@gmail.com>
2019-01-10 09:30:11 -0500

    setup prerelease builds

---
+++ scripts/updatePreReleaseVersion.js
@@ -0,0 +1,25 @@
+// This is only for use in CI to set the version number in package.json
+// for publishing prerelease versions to npm
+
+const { spawnSync } = require('child_process')
+const { writeFileSync, readFileSync } = require('fs')
+const semver = require('semver')
+
+const packageJson = JSON.parse(readFileSync('package.json').toString())
+
+const version = semver.valid(
+  spawnSync('git', ['describe', '--tags'])
+    .stdout.toString()
+    .trimEnd()
+)
+
+if (!semver.gt(packageJson.version, version)) {
+  console.log(packageJson.version, version)
+  packageJson.version = version
+  writeFileSync('package.json', JSON.stringify(packageJson, null, 2) + '\n')
+} else {
+  console.error(`Error: ${packageJson.version} greater than ${version}
+You need to make a tagged release or pre-release to continue 
+making automatic prereleases after updating the version number`)
+  process.exit(1)
+}
