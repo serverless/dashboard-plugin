d4b1f16fc5425b7aacbe69ae16568d56170330b9
Daniel Schep <dschep@gmail.com>
2019-02-12 11:46:15 -0500

    Don't break updateDeploy when service doesn't have an API GW

--- src/lib/updateDeployment.test.js
+++ src/lib/updateDeployment.test.js
@@ -75,4 +75,59 @@ describe('updateDeployment', () => {
       name: 'servicename'
     })
   })
+
+  it('sends new deployments to the platform backend without APIG', async () => {
+    const log = jest.fn()
+    const getStackResources = jest.fn().mockReturnValue(Promise.resolve([]))
+    const getAccountId = jest.fn().mockReturnValue(Promise.resolve('accountId'))
+    const getStackName = jest.fn().mockReturnValue('stackName')
+    const getServiceEndpointRegex = jest.fn().mockReturnValue('ServiceEndpoint')
+    const request = jest.fn().mockReturnValue(Promise.resolve({ Stacks: [{ Outputs: [] }] }))
+    const ctx = {
+      sls: {
+        service: {
+          tenant: 'tenantname',
+          app: 'appname',
+          service: 'servicename'
+        },
+        cli: { log }
+      },
+      provider: {
+        getStackResources,
+        getAccountId,
+        request,
+        naming: { getStackName, getServiceEndpointRegex }
+      },
+      state: {
+        deployment: {
+          deploymentId: 'deploymentId'
+        }
+      }
+    }
+    await updateDeploymentLocal(ctx)
+
+    expect(log).toBeCalledWith(
+      'Publishing service to the Enterprise Dashboard...',
+      'Serverless Enterprise'
+    )
+    expect(getAccessKeyForTenant).toBeCalledWith('tenantname')
+    expect(updateDeployment).toBeCalledWith({
+      accessKey: 'accessKey',
+      app: 'appname',
+      computedData: {
+        accountId: 'accountId',
+        apiId: 'API_ID',
+        physicalIds: []
+      },
+      deploymentId: 'deploymentId',
+      serviceName: 'servicename',
+      status: 'success',
+      tenant: 'tenantname'
+    })
+    expect(getServiceUrl).toBeCalledWith({
+      tenant: 'tenantname',
+      app: 'appname',
+      name: 'servicename'
+    })
+  })
 })
