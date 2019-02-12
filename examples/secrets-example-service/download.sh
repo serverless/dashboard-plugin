acc9a0b68882b90234a31ac9f49260aba60d1cc7
Jeremy Coffield <jeremy@serverless.com>
2019-02-12 14:01:53 -0600

    Change download script not to cd into dir

--- examples/secrets-example-service/download.sh
+++ examples/secrets-example-service/download.sh
@@ -1,7 +1,5 @@
 mkdir secrets-example-service
-pushd secrets-example-service
-curl https://raw.githubusercontent.com/serverless/enterprise-plugin/master/examples/secrets-example-service/package.json --output package.json
-curl https://raw.githubusercontent.com/serverless/enterprise-plugin/master/examples/secrets-example-service/serverless.yml --output serverless.yml
-curl https://raw.githubusercontent.com/serverless/enterprise-plugin/master/examples/secrets-example-service/handler.js --output handler.js
-npm install
-popd
+curl https://raw.githubusercontent.com/serverless/enterprise-plugin/master/examples/secrets-example-service/package.json --output secrets-example-service/package.json
+curl https://raw.githubusercontent.com/serverless/enterprise-plugin/master/examples/secrets-example-service/serverless.yml --output secrets-example-service/serverless.yml
+curl https://raw.githubusercontent.com/serverless/enterprise-plugin/master/examples/secrets-example-service/handler.js --output secrets-example-service/handler.js
+npm install --prefix secrets-example-service
