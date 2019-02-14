b7fbaa97582158c5e7a63e9e20bae44b2e39b95e
Daniel Schep <dschep@gmail.com>
2019-02-14 13:53:25 -0500

    PLAT-603 allowed runtimes safeguard

---
+++ src/lib/safeguards/policies/allowed-runtimes.js
@@ -0,0 +1,16 @@
+module.exports = function allowedRuntimesPolicy(policy, service, allowedRuntimes) {
+  for (const fnName in service.declaration.functions || {}) {
+    if (
+      !allowedRuntimes.includes(
+        service.declaration.functions[fnName].runtime || service.declaration.provider.runtime
+      )
+    ) {
+      throw new policy.Failure(
+        `Runtime of function ${fnName} not in list of permitted runtimes: ${JSON.stringify(
+          allowedRuntimes
+        )}`
+      )
+    }
+  }
+  policy.approve()
+}
