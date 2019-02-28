51a434a861385ef4f2bdabcbab9fdbb7e3a357a0
Daniel Schep <dschep@gmail.com>
2019-02-28 14:08:28 -0500

    [PLAT-596] require description safeguard
    Depends on #49

---
+++ src/lib/safeguards/policies/require-description.js
@@ -0,0 +1,46 @@
+const { fromPairs } = require('lodash')
+
+module.exports = function requireDescriptionPolicy(policy, service, options) {
+  let failed = false
+  const {
+    declaration: { functions },
+    provider: { naming },
+    compiled: {
+      'cloudformation-template-update-stack.json': { Resources }
+    }
+  } = service
+  const logicalFuncNamesToConfigFuncName = fromPairs(
+    Object.keys(functions || {}).map((funcName) => [naming.getLambdaLogicalId(funcName), funcName])
+  )
+
+  // for (const [name, { events, onError }] of Object.entries(functions)) {
+  for (const [funcName, { Properties, Type }] of Object.entries(Resources)) {
+    if (Type !== 'AWS::Lambda::Function') {
+      continue
+    }
+    if (!Properties.Description) {
+      failed = true
+      policy.fail(`Function "${logicalFuncNamesToConfigFuncName[funcName]}" has no description`)
+    } else {
+      if (Properties.Description.length > ((options && options.maxLength) || 256)) {
+        failed = true
+        policy.fail(
+          `Description for function "${logicalFuncNamesToConfigFuncName[funcName]}" is too long`
+        )
+      }
+      if (Properties.Description.length < ((options && options.minLength) || 30)) {
+        failed = true
+        policy.fail(
+          `Description for function "${logicalFuncNamesToConfigFuncName[funcName]}" is too short`
+        )
+      }
+    }
+  }
+
+  if (!failed) {
+    policy.approve()
+  }
+}
+
+module.exports.docs =
+  'https://github.com/serverless/enterprise/blob/master/docs/safeguards.md#ensure-dead-letter-queues-are-attached-to-functions'
