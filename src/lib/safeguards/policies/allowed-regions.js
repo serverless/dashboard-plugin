cf07ae7f3509831634573e9189cc735da70cf82e
Daniel Schep <dschep@gmail.com>
2019-02-28 13:42:26 -0500

    update for 8439b4d

--- src/lib/safeguards/policies/allowed-regions.js
+++ src/lib/safeguards/policies/allowed-regions.js
@@ -1,11 +1,12 @@
 module.exports = function allowedRegionsPolicy(policy, service, options) {
   const region = service.provider.getRegion()
   if (!options.includes(region)) {
-    throw new policy.Failure(
+    policy.fail(
       `Region "${region}" not in list of permitted regions: ${JSON.stringify(options)}`
     )
+  } else {
+    policy.approve()
   }
-  policy.approve()
 }
 
 module.exports.docs =
--- src/lib/safeguards/policies/allowed-regions.test.js
+++ src/lib/safeguards/policies/allowed-regions.test.js
@@ -4,30 +4,30 @@ describe('allowedRegionsPolicy', () => {
   let policy
 
   beforeEach(() => {
-    policy = { approve: jest.fn(), warn: jest.fn(), Failure: Error }
+    policy = { approve: jest.fn(), fail: jest.fn() }
   })
 
   it('allows us-east-1 with options ["us-east-1"]', () => {
     const getRegion = jest.fn().mockReturnValue('us-east-1')
     allowedRegionsPolicy(policy, { provider: { getRegion } }, ['us-east-1'])
     expect(policy.approve).toHaveBeenCalledTimes(1)
-    expect(policy.warn).toHaveBeenCalledTimes(0)
+    expect(policy.fail).toHaveBeenCalledTimes(0)
   })
 
   it('allows us-east-1 with options ["us-east-1", "us-east-2"]', () => {
     const getRegion = jest.fn().mockReturnValue('us-east-1')
     allowedRegionsPolicy(policy, { provider: { getRegion } }, ['us-east-1', 'us-east-2'])
     expect(policy.approve).toHaveBeenCalledTimes(1)
-    expect(policy.warn).toHaveBeenCalledTimes(0)
+    expect(policy.fail).toHaveBeenCalledTimes(0)
   })
 
   it('forbids us-east-1 with options ["us-east-2"]', () => {
     const getRegion = jest.fn().mockReturnValue('us-east-1')
 
-    expect(() => allowedRegionsPolicy(policy, { provider: { getRegion } }, ['us-east-2'])).toThrow(
+    allowedRegionsPolicy(policy, { provider: { getRegion } }, ['us-east-2'])
+    expect(policy.approve).toHaveBeenCalledTimes(0)
+    expect(policy.fail).toBeCalledWith(
       'Region "us-east-1" not in list of permitted regions: ["us-east-2"]'
     )
-    expect(policy.approve).toHaveBeenCalledTimes(0)
-    expect(policy.warn).toHaveBeenCalledTimes(0)
   })
 })
