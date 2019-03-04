8bb17bfb1ac4444922fdf3b1d35f192468e0b29d
Daniel Schep <dschep@gmail.com>
2019-03-04 10:16:00 -0500

    lint

--- src/lib/safeguards/policies/restricted-deploy-times.test.js
+++ src/lib/safeguards/policies/restricted-deploy-times.test.js
@@ -1,13 +1,12 @@
 import restrictedDeployTimesPolicy from './restricted-deploy-times'
 
-
 describe('restrictedDeployTimesPolicy', () => {
   const RealDate = Date
   let policy
 
-  function mockDate (isoDate) {
+  function mockDate(isoDate) {
     global.Date = class extends RealDate {
-      constructor () {
+      constructor() {
         return new RealDate(isoDate)
       }
     }
@@ -29,21 +28,21 @@ describe('restrictedDeployTimesPolicy', () => {
 
   it('allow blocks deployment on Friday when Friday is blocked', () => {
     mockDate('2019-03-01T01:00:00')
-    restrictedDeployTimesPolicy(policy, {}, { blockedWeekdays: [ 'Friday' ] })
+    restrictedDeployTimesPolicy(policy, {}, { blockedWeekdays: ['Friday'] })
     expect(policy.approve).toHaveBeenCalledTimes(0)
     expect(policy.fail).toBeCalledWith('Deploying on fr is not allowed')
   })
 
   it('allow deployment on Monday when Friday is blocked', () => {
     mockDate('2019-03-04T01:00:00')
-    restrictedDeployTimesPolicy(policy, {}, { blockedWeekdays: [ 'Friday' ] })
+    restrictedDeployTimesPolicy(policy, {}, { blockedWeekdays: ['Friday'] })
     expect(policy.approve).toHaveBeenCalledTimes(1)
     expect(policy.fail).toHaveBeenCalledTimes(0)
   })
 
   it('blocks deployment on xmas when it is blocked', () => {
     mockDate('2019-12-25T01:00:00')
-    restrictedDeployTimesPolicy(policy, {}, { blockedDates: [ '12/25/2019' ] })
+    restrictedDeployTimesPolicy(policy, {}, { blockedDates: ['12/25/2019'] })
     expect(policy.approve).toHaveBeenCalledTimes(0)
     expect(policy.fail).toBeCalledWith('Deploying on 12/25/2019 is not allowed')
   })
