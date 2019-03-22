6553bc45630a7c1524ee66b16c7abeee25224e36
Daniel Schep <dschep@gmail.com>
2019-03-22 11:03:27 -0400

    lint

--- src/lib/safeguards/policies/allowed-regions.js
+++ src/lib/safeguards/policies/allowed-regions.js
@@ -1,9 +1,7 @@
 module.exports = function allowedRegionsPolicy(policy, service, options) {
   const region = service.provider.getRegion()
   if (!options.includes(region)) {
-    policy.fail(
-      `Region "${region}" not in list of permitted regions: ${JSON.stringify(options)}`
-    )
+    policy.fail(`Region "${region}" not in list of permitted regions: ${JSON.stringify(options)}`)
   } else {
     policy.approve()
   }
