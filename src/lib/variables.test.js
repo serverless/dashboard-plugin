eb24c51f859b6402a7062bb25a1c4bc35cef9fe9
Daniel Schep <dschep@gmail.com>
2019-05-09 10:24:39 -0400

    fix variables test

--- src/lib/variables.test.js
+++ src/lib/variables.test.js
@@ -1,31 +1,4 @@
-import { getSecret, getAccessKeyForTenant } from '@serverless/platform-sdk'
-import { getSecretFromEnterprise, hookIntoVariableGetter } from './variables'
-
-jest.mock('@serverless/platform-sdk', () => ({
-  getSecret: jest.fn().mockReturnValue(Promise.resolve({ secretValue: 'SECRET!' })),
-  getAccessKeyForTenant: jest.fn().mockReturnValue(Promise.resolve('accessKey'))
-}))
-
-afterAll(() => jest.restoreAllMocks())
-
-describe('variables - getSecretFromEnterprise', () => {
-  it('gets the access key and grabs the secret from backend', async () => {
-    await getSecretFromEnterprise({
-      secretName: 'secrets:name',
-      app: 'app',
-      service: 'service',
-      tenant: 'tenant'
-    })
-    expect(getAccessKeyForTenant).toBeCalledWith('tenant')
-    expect(getSecret).toBeCalledWith({
-      secretName: 'secrets:name',
-      accessKey: 'accessKey',
-      app: 'app',
-      service: 'service',
-      tenant: 'tenant'
-    })
-  })
-})
+import { hookIntoVariableGetter } from './variables'
 
 describe('variables - hookIntoVariableGetter', () => {
   const getValueFromSource = jest.fn().mockReturnValue('frameworkVariableValue')
@@ -47,17 +20,9 @@ describe('variables - hookIntoVariableGetter', () => {
   })
 
   it('overrides the default variable getter', async () => {
-    const restore = hookIntoVariableGetter({ sls: serverless, state })
+    const restore = hookIntoVariableGetter({ sls: serverless, state }, { name: 'secretValue' })
     expect(serverless.variables.getValueFromSource).not.toEqual(getValueFromSource)
     serverless.variables.getValueFromSource('secrets:name')
-    expect(getAccessKeyForTenant).toBeCalledWith('tenant')
-    expect(getSecret).toBeCalledWith({
-      secretName: 'secrets:name',
-      accessKey: 'accessKey',
-      app: 'app',
-      service: 'service',
-      tenant: 'tenant'
-    })
     expect(state.secretsUsed).toEqual(new Set(['name']))
     restore()
     expect(serverless.variables.getValueFromSource).toEqual(getValueFromSource)
