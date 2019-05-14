9a636c53d14925a59fe734c91f19ef2b079ade80
Daniel Schep <dschep@gmail.com>
2019-05-14 17:17:14 -0400

    PLAT-1003 - gentler presentation of "you musth finish signup" error message

--- src/lib/login.js
+++ src/lib/login.js
@@ -4,8 +4,14 @@ export default async function(ctx) {
   ctx.sls.cli.log('Logging you in via your default browser...', 'Serverless Enterprise')
   // Include a "tenant" in "login()"...
   // This will create a new accessKey for that tenant on every login.
-  return login(ctx.sls.service.tenant).then(() => {
-    ctx.sls.cli.log('You sucessfully logged in to Serverless Enterprise.', 'Serverless Enterprise')
-    process.exit(0)
-  })
+  try {
+    await login(ctx.sls.service.tenant)
+  } catch (err) {
+    if (err === 'Complete sign-up before logging in.') {
+      ctx.sls.cli.log(err, 'Serverless Enterprise')
+      process.exit(1)
+    }
+  }
+  ctx.sls.cli.log('You sucessfully logged in to Serverless Enterprise.', 'Serverless Enterprise')
+  process.exit(0)
 }
