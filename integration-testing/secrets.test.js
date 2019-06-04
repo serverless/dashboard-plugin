85b33ed0b794f8965ff9358585975e84d0386912
Daniel Schep <dschep@gmail.com>
2019-06-04 08:13:41 -0400

    forgot path import

--- integration-testing/secrets.test.js
+++ integration-testing/secrets.test.js
@@ -1,6 +1,8 @@
+import path from 'path'
 import { npm, sls } from './commands'
 
 const SERVERLESS_PLATFORM_STAGE = process.env.SERVERLESS_PLATFORM_STAGE || 'dev'
+const cwd = path.join(__dirname, 'service2')
 
 beforeAll(() => npm(['install']))
 
@@ -8,7 +10,8 @@ describe('integration', () => {
   it('print contains the secret in the deploy profile', () => {
     const proc = sls(['print', '--path', 'custom.testSecret'], {
       stdio: 'pipe',
-      env: { ...process.env, SERVERLESS_PLATFORM_STAGE, EVENTS: 'noEvents' }
+      env: { ...process.env, SERVERLESS_PLATFORM_STAGE },
+      cwd
     })
     const stdout = proc.stdout.toString()
     expect(stdout).toMatch(/^testSecretValue$/m)
