91a0a7a1f73b8d5ef01e5cf5cfb4a03dfa66abdf
Alex DeBrie <alexdebrie1@gmail.com>
2019-06-25 14:30:38 -0500

    Configure APIGW access logs subscription filter

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -16,4 +16,24 @@ function pickResourceType(template, resourcesType) {
   return resources
 }
 
-export { upperFirst, pickResourceType }
+const API_GATEWAY_FILTER_PATTERN = JSON.stringify({
+  requestId: '$context.requestId',
+  apiId: '$context.apiId',
+  resourceId: '$context.resourceId',
+  resourcePath: '$context.resourcePath',
+  path: '$context.path',
+  httpMethod: '$context.httpMethod',
+  status: '$context.status',
+  authLatency: '$context.authorizer.integrationLatency',
+  integrationLatency: '$context.integrationLatency',
+  integrationStatus: '$context.integrationStatus',
+  responseLatency: '$context.responseLatency',
+  responseLength: '$context.responseLength',
+  errorMessage: '$context.error.message',
+  requestTime: '$context.requestTime',
+  format: 'sls-access-log',
+  version: '1.0.0'
+})
+const LAMBDA_FILTER_PATTERN = '?"REPORT RequestId: " ?"SERVERLESS_ENTERPRISE"'
+
+export { upperFirst, pickResourceType, API_GATEWAY_FILTER_PATTERN, LAMBDA_FILTER_PATTERN }
