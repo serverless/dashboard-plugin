8ad5fac12cb9dab937b7e66b7c3f83dd6805762e
Mariusz Nowak <mariusz@medikoo.com>
2019-07-02 14:34:21 +0200

    Do not test content of sdk-js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   coverageDirectory: './coverage/',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js'],
-  testPathIgnorePatterns: ['/dist', '/node_modules'],
+  testPathIgnorePatterns: ['/dist', '/node_modules', '/sdk-js'],
   testEnvironment: 'node',
   setupFiles: ['<rootDir>/src/runtime.js']
 }
