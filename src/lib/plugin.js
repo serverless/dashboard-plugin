a8f1712bb5d26cc4d94ca93ab51c3aec36e3b86d
Daniel Schep <dschep@gmail.com>
2019-07-09 13:09:06 -0400

    Forward compatibility w/ orgs and update all user facing copy to use org too

--- src/lib/plugin.js
+++ src/lib/plugin.js
@@ -40,10 +40,13 @@ class ServerlessEnterprisePlugin {
         )
     }
 
+    // forward compatibility with org
+    sls.service.tenant = sls.service.org || sls.service.tenant
+
     // Check if Enterprise is configured
     const missing = []
     if (!sls.service.tenant) {
-      missing.push('tenant')
+      missing.push('org')
     }
     if (!sls.service.app) {
       missing.push('app')
@@ -59,7 +62,7 @@ class ServerlessEnterprisePlugin {
         currentCommand !== 'logout' &&
         !process.env.SERVERLESS_ACCESS_KEY)
     ) {
-      if (missing.includes('tenant') && missing.includes('app')) {
+      if (missing.includes('org') && missing.includes('app')) {
         return // user isn't trying to use SFE
       }
       const errorMessage = `You are not currently logged in. To log in, use: $ serverless login`
