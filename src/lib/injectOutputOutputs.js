b2bed40eb5632cc208a758989e01ca800307d129
Daniel Schep <dschep@gmail.com>
2019-07-10 14:21:14 -0400

    include the output injection file and add test coverage

---
+++ src/lib/injectOutputOutputs.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { entries, values } = require('lodash');
+
+module.exports = async function(ctx) {
+  for (const [outputKey, outputValue] of entries(ctx.sls.service.outputs || {})) {
+    if (typeof outputValue === 'object') {
+      const outputValueKeys = Object.keys(outputValue);
+      if (outputValueKeys.includes('Ref') || outputValueKeys.includes('Fn::GetAtt')) {
+        ctx.sls.service.provider.compiledCloudFormationTemplate.Outputs[`SFEOutput${outputKey}`] = {
+          Description: `SFE output "${outputKey}"`,
+          Value: outputValue,
+        };
+        ctx.sls.service.outputs[outputKey] = `CFN!?${outputKey}`;
+      }
+    }
+  }
+};
