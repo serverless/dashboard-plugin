8972ff2fb8a9d8d47675d6a7bf79841d162fbd86
Mariusz Nowak <mariusz@medikoo.com>
2019-07-11 10:10:35 +0200

    Do not include npm install in build step

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,5 @@ rm -rf dist
 mkdir -p dist
 babel src -d dist --source-maps --ignore "**/*.test.js"
 rsync -avz --exclude "*.js" --exclude "__tests__" --exclude "node_modules" src/ dist/
-cd sdk-js && npm i && npm run build
+cd sdk-js
+npm run build
