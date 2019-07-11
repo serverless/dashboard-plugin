a8b46b213eda0e915caf4284c43e1c577b6ec300
Mariusz Nowak <mariusz@medikoo.com>
2019-07-11 10:11:15 +0200

    Update after deletion of src directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,12 @@
 set -e
 rm -rf dist
 mkdir -p dist
-babel src -d dist --source-maps --ignore "**/*.test.js"
-rsync -avz --exclude "*.js" --exclude "__tests__" --exclude "node_modules" src/ dist/
+babel index.js -o dist/index.js --source-maps
+babel runtime.js -o dist/runtime.js --source-maps
+babel lib -d dist/lib --source-maps --ignore "**/*.test.js"
+cp -a package.json dist/package.json
 cd sdk-js
 npm run build
+cd ..
+mkdir -p dist/sdk-js/dist
+cp -a sdk-js/dist/index.js dist/sdk-js/dist/index.js
