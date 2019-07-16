d6d4234e156080b45d797ba57197e78be83d50e9
Daniel Schep <dschep@gmail.com>
2019-07-16 16:21:50 -0400

    oh damnit

--- scripts/updatePreReleaseVersion.js
+++ scripts/updatePreReleaseVersion.js
@@ -14,6 +14,8 @@ const version = semver.valid(
     .stdout.toString()
     .slice(0, -1)
 );
+
+// eslint-disable-next-line no-console
 console.log(version);
 
 if (packageJson.version === version && !process.env.TRAVIS_TAG) {
