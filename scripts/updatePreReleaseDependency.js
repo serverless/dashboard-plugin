30970cc36cce8c95e01aa9c46871e54e4abe5f85
Mariusz Nowak <mariusz@medikoo.com>
2019-07-16 22:54:52 +0200

    Add notice on invocation environment

--- scripts/updatePreReleaseDependency.js
+++ scripts/updatePreReleaseDependency.js
@@ -3,6 +3,8 @@
 // This is only for use in CI to set the version of platform-sdk in package.json
 // for publishing prerelease versions to npm
 
+// Important: It's run before `npm install`, we cannot rely on non native modules
+
 const { writeFileSync, readFileSync } = require('fs');
 
 const packageJson = JSON.parse(readFileSync('package.json').toString());
