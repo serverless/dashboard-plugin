9476067ea8e4111fc8a1f8557ecc88498432a021
Daniel Schep <dschep@gmail.com>
2019-10-07 10:36:59 -0400

    back to vendored

--- integration-testing/wrapper-service/handler.py
+++ integration-testing/wrapper-service/handler.py
@@ -1,15 +1,11 @@
 import boto3
 from botocore.vendored import requests as boto_vendored_requests
 
-import sys
-sys.path.append('/var/task/vendor')
-import requests
-
 def success(event, context):
     boto3.client('sts').get_caller_identity()
-    requests.get('https://httpbin.org/get')
+    boto_vendored_requests.get('https://httpbin.org/get')
     try:
-        requests.get("https://asdfkasdjsdf")
+        boto_vendored_requests.get("https://asdfkasdjsdf")
     except:
         pass
     return 'success'
