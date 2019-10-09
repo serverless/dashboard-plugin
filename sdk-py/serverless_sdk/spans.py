a465046abf7e3df04e2fd214edb72753e71e09f2
Daniel Schep <dschep@gmail.com>
2019-10-09 09:16:39 -0400

    async context manager

--- sdk-py/serverless_sdk/spans.py
+++ sdk-py/serverless_sdk/spans.py
@@ -1,7 +1,15 @@
 import time
 from datetime import datetime
 
+try:
+    from serverless_sdk.make_context_manager_async import async_context_manager
+except SyntaxError:
+    # Python 2 doesn't support `async def`
+    def async_context_manager(context_manager):
+        return context_manager
 
+
+@async_context_manager
 class Span(object):
     def __init__(self, emmiter, span_type):
         self.emmiter = emmiter
@@ -25,7 +33,7 @@ class Span(object):
 
         return self
 
-    def __exit__(self, *args):
+    def __exit__(self, exc_type, exc, tb):
         self.end_isoformat = datetime.utcnow().isoformat() + "Z"
         self.end = time.time()
         self.emmiter(self.dump())
