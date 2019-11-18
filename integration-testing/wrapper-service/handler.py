910d90c4f5af7ac216e5aadbad71bf8bc89b1bc6
astuyve <astuyve@gmail.com>
2019-11-18 14:22:49 -0600

    Integration tests. Prettify

--- integration-testing/wrapper-service/handler.py
+++ integration-testing/wrapper-service/handler.py
@@ -1,10 +1,9 @@
 import time
 import boto3
 from botocore.vendored import requests
-from serverless_sdk import tag_event, span
 
 def success(event, context):
-    with span('create sts client'):
+    with context.serverless_sdk.span('create sts client'):
         sts = boto3.client('sts')
     sts.get_caller_identity()
     requests.get('https://httpbin.org/get')
@@ -21,7 +20,7 @@ def http_error(event, context):
     return 'http_erroO'
 
 def event_tags(event, context):
-    tag_event('event-tagged', 'true', { 'customerId': 5, 'userName': 'aaron.stuyvenberg'})
+    context.serverless_sdk.tag_event('event-tagged', 'true', { 'customerId': 5, 'userName': 'aaron.stuyvenberg'})
     return 'success'
 
 def timeout(event, context):
