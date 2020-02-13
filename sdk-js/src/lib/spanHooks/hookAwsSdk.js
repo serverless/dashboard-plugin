48e9702736c70086f728c46754d57297c40d3ae8
Michael Kazmier <dakazmier@gmail.com>
2020-02-13 08:39:46 -0700

    Allow users to skip AWS_SDK span collections with env var

--- sdk-js/src/lib/spanHooks/hookAwsSdk.js
+++ sdk-js/src/lib/spanHooks/hookAwsSdk.js
@@ -8,7 +8,11 @@ module.exports = emitter => {
     for (const Service of Object.values(awsSdk)) {
       if (Service.serviceIdentifier) {
         Service.prototype.customizeRequests(req => {
-          req.on('complete', res => emitter.emit('span', captureAwsRequestSpan(res)));
+          req.on('complete', res => {
+            if (!process.env.SERVERLESS_ENTERPRISE_SPANS_IGNORE_AWS_SDK) {
+              emitter.emit('span', captureAwsRequestSpan(res));
+            }
+          });
           return req;
         });
       }
