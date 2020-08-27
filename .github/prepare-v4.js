9d55b9c5fafea8fb61a2b5459927d610c4c34e46
Mariusz Nowak <medyk@medikoo.com>
2020-08-27 15:57:43 +0200

    chore: Prepare temporary v4 branch

--- .github/prepare-v4.js
+++ .github/prepare-v4.js
@@ -9,6 +9,6 @@ const packageJsonPath = path.resolve(__dirname, '../package.json');
 
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
-packageJson.version = `${packageJson.version}-${process.env.GITHUB_SHA.slice(0, 8)}`;
+packageJson.version = `${packageJson.version}.${process.env.GITHUB_SHA.slice(0, 8)}`;
 
 fs.writeFileSync(packageJsonPath, `${JSON.stringify(packageJson, null, 2)}\n`);
