a07c915ddb20b75f6717b2ed705a8ba35a4aca55
Mariusz Nowak <medyk@medikoo.com>
2020-11-04 10:47:42 +0100

    test: Upgrade to @serverless/test v5

--- test/run-serverless.js
+++ test/run-serverless.js
@@ -2,5 +2,30 @@
 
 const runServerless = require('@serverless/test/run-serverless');
 const setupServerless = require('./setupServerless');
+const fixtures = require('./fixtures');
 
-module.exports = async (options) => runServerless((await setupServerless()).root, options);
+module.exports = async (options) => {
+  if (options.fixture && options.cwd) {
+    throw new Error('Either "fixture" or "cwd" should be provided');
+  }
+  const runServerlessOptions = Object.assign({}, options);
+  delete runServerlessOptions.serverlessPath;
+  delete runServerlessOptions.fixture;
+  delete runServerlessOptions.configExt;
+  let fixtureData;
+  if (options.fixture) {
+    fixtureData = await fixtures.setup(options.fixture, { configExt: options.configExt });
+    runServerlessOptions.cwd = fixtureData.servicePath;
+  }
+  try {
+    const result = await runServerless(
+      options.serverlessPath || (await setupServerless()).root,
+      runServerlessOptions
+    );
+    result.fixtureData = fixtureData;
+    return result;
+  } catch (error) {
+    error.fixtureData = fixtureData;
+    throw error;
+  }
+};
