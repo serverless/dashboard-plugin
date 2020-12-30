3d5005653734c15caeb3081daf67e08e07a9d32d
Mariusz Nowak <medyk@medikoo.com>
2020-12-30 14:50:16 +0100

    test: Get rid of global vars mapper
    It was introduced initially to ease migration from Jest.
    Now it appers it doesn't work well with Mocha v8.
    Updated tests code to be fully compliant with Mocha

--- integration-testing/secrets.test.js
+++ integration-testing/secrets.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { expect } = require('chai');
 const stripAnsi = require('strip-ansi');
 const setup = require('./setup');
 
@@ -8,7 +9,7 @@ let sls;
 describe('integration: secrets', function () {
   this.timeout(1000 * 60 * 3);
 
-  beforeAll(async () => ({ sls } = await setup('service2')));
+  before(async () => ({ sls } = await setup('service2')));
 
   it('print contains the secret in the deploy profile', async () => {
     const stdout = stripAnsi(
