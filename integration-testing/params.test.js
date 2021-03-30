ad694dc30ec14bd58d0181589a073ad6ebad9975
Mariusz Nowak <medyk@medikoo.com>
2021-03-30 08:49:15 +0200

    test: Refactor to use current naming

--- integration-testing/params.test.js
+++ integration-testing/params.test.js
@@ -6,14 +6,14 @@ const setup = require('./setup');
 
 let sls;
 
-describe('integration: secrets', function () {
+describe('integration: params', function () {
   this.timeout(1000 * 60 * 3);
 
   before(async () => ({ sls } = await setup('service2')));
 
-  it('print contains the secret in the deploy profile', async () => {
+  it('print contains the params in the deploy profile', async () => {
     const stdout = stripAnsi(
-      String((await sls(['print', '--path', 'custom.testSecret'])).stdoutBuffer)
+      String((await sls(['print', '--path', 'custom.testParam'])).stdoutBuffer)
     );
     expect(stdout).to.include('testSecretValue\n\n');
   });
