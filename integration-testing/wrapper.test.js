26e6b65d3566d76e5869ec888cfc82b030770289
Mariusz Nowak <medyk@medikoo.com>
2021-11-08 15:42:48 +0100

    test: Remove testing of nodejs10.x runtime
    It's no longer supported by AWS

--- integration-testing/wrapper.test.js
+++ integration-testing/wrapper.test.js
@@ -439,63 +439,6 @@ describe('integration: wrapper', () => {
     });
   });
 
-  it('gets spans in node 10', async () => {
-    const functionName = `${serviceName}-dev-spans10`;
-    const { LogResult } = await awsRequest(lambdaService, 'invoke', {
-      LogType: 'Tail',
-      FunctionName: functionName,
-    });
-    const payload = await resolveAndValidateLog(functionName, LogResult);
-    expect(payload.type).to.equal('transaction');
-    expect(payload.payload.spans.length).to.equal(5);
-    // first custom span (create sts client)
-    expect(new Set(Object.keys(payload.payload.spans[0]))).to.deep.equal(
-      new Set(['duration', 'endTime', 'startTime', 'tags'])
-    );
-    expect(payload.payload.spans[0].tags).to.deep.equal({
-      type: 'custom',
-      label: 'create sts client',
-    });
-    // aws span
-    expect(new Set(Object.keys(payload.payload.spans[1]))).to.deep.equal(
-      new Set(['duration', 'endTime', 'startTime', 'tags'])
-    );
-    expect(new Set(Object.keys(payload.payload.spans[1].tags.aws))).to.deep.equal(
-      new Set(['errorCode', 'operation', 'region', 'requestId', 'service'])
-    );
-    expect(payload.payload.spans[1].tags.type).to.equal('aws');
-    // first http span (POST w/ https.request)
-    expect(new Set(Object.keys(payload.payload.spans[2]))).to.deep.equal(
-      new Set(['duration', 'endTime', 'startTime', 'tags'])
-    );
-    expect(payload.payload.spans[2].tags).to.deep.equal({
-      type: 'http',
-      requestHostname: 'httpbin.org',
-      requestPath: '/post',
-      httpMethod: 'POST',
-      httpStatus: 200,
-    });
-    // second http span (https.get)
-    expect(new Set(Object.keys(payload.payload.spans[3]))).to.deep.equal(
-      new Set(['duration', 'endTime', 'startTime', 'tags'])
-    );
-    expect(payload.payload.spans[3].tags).to.deep.equal({
-      type: 'http',
-      requestHostname: 'httpbin.org',
-      requestPath: '/get',
-      httpMethod: 'GET',
-      httpStatus: 200,
-    });
-    // second custom span (asynctest)
-    expect(new Set(Object.keys(payload.payload.spans[4]))).to.deep.equal(
-      new Set(['duration', 'endTime', 'startTime', 'tags'])
-    );
-    expect(payload.payload.spans[4].tags).to.deep.equal({
-      type: 'custom',
-      label: 'asynctest',
-    });
-  });
-
   it('gets SFE log msg from wrapped node timeout handler', async () => {
     const functionName = `${serviceName}-dev-timeout`;
     const { LogResult } = await awsRequest(lambdaService, 'invoke', {
